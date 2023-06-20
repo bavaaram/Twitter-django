a14f5d47e618e5c9a90f0dd5b628776a8b8e70f8
bavaaram <itsjustabavaar@gmail.com>
2023-06-20 17:10:25 +0330

    Add a new function in views.py in post app

--- post/views.py
+++ post/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.get_posts()
+    context = {'posts': posts}
+    return render(request, 'post_list.html', context)
 
 # Create your views here.
