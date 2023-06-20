4d9b577e7d4431e6b34db1af35133800e57f8638
bavaaram <itsjustabavaar@gmail.com>
2023-06-20 17:13:35 +0330

    Register posts app models to admin.py

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Post, Comment, Tag, Reaction, Image
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text']
+    search_fields = ['username', 'text']
+    inlines = [ImageInline]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reaction)
+class ReactionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
 # Register your models here.
