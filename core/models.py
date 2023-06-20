980d498a3a30233491d5397e8e2841cb0960603a
bavaaram <itsjustabavaar@gmail.com>
2023-06-20 17:05:17 +0330

    Add some new features in base abstract models in core app

--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
 from django.utils.translation import gettext as _
 
 # Create your models here.
