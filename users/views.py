eabe8d34b6ee5b3200bb57260beb72634db46820
bavaaram <itsjustabavaar@gmail.com>
2023-06-20 17:03:35 +0330

    Add a new function in views.py called user_list for showing list of users in users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import User
+
+
+def user_list(request):
+    users = User.get_users()
+    context = {'users': users}
+    return render(request, 'user_list.html', context)
 
 # Create your views here.
