5c36672bc18222233d10abad3d0ad2e9c896016d
bavaaram <itsjustabavaar@gmail.com>
2023-06-20 16:55:51 +0330

    Adding some methods and features to users app models

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from uuid import uuid4
 from django.utils.translation import gettext as _
 
+
 class User(models.Model):
     username = models.CharField(_("Username"), max_length=50)
     image = models.ImageField(_("Profile Image"), upload_to='image')
@@ -101,4 +102,40 @@ class Archive(models.Model):
     def __str__(self):
         return self.post_id
 
-# Create your models here.
+    def archive_post(self, post):
+        archive, created = Archive.objects.get_or_create(user_archive=self, post_archive=post)
+        if created:
+            return True
+        return False
+
+class Relation(models.Model):
+    from_user = models.ForeignKey("User",
+                                  verbose_name=_("from_user"),
+                                  on_delete=models.CASCADE,
+                                  related_name="Following")
+    to_user = models.ForeignKey("User",
+                                verbose_name=_("from_user"),
+                                on_delete=models.CASCADE,
+                                related_name="Followers")
+    create_at = models.DateTimeField(_("create at"), auto_now=False, auto_now_add=True)
+    update_at = models.DateTimeField(_("update at"), auto_now=True, auto_now_add=False)
+
+    def followers(self):
+        followers = Relation.objects.filter(to_user=self)
+        return [follower.from_user for follower in followers]
+
+    def follow(self, user):
+        if user != self:
+            relation, created = Relation.objects.get_or_create(from_user=self, to_user=user)
+            if created:
+                return True
+        return False
+
+    def unfollow(self, user):
+        relation = Relation.objects.filter(from_user=self, to_user=user).first()
+        if relation:
+            relation.delete()
+            return True
+        return False
+
+    # Create your models here.
