140d0cf2b165b9a103d5517d4d59c54e78ae501a
bavaaram <itsjustabavaar@gmail.com>
2023-06-20 17:01:47 +0330

    Reigster User and Archive and Relation models in admin.py file in users app

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, Archive, Relation
 
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'biography']
+    search_fields = ['username']
+
+
+@admin.register(Relation)
+class RelationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Archive)
+class ArchiveAdmin(admin.ModelAdmin):
+    pass
 # Register your models here.
