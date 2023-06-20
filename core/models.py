5ec5fffab4c32c6b7f93e03dde2459ddcdf6407a
bavaaram <itsjustabavaar@gmail.com>
2023-06-20 16:49:05 +0330

    Add some base models to core app

--- core/models.py
+++ core/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.utils.translation import gettext as _
 
+from django.db import models
+from uuid import uuid4
+from django.utils.translation import gettext as _
+
+
+# Create your models here.
+
+class BaseModel(models.Model):
+    id = models.UUIDField(_("id"), editable=False, primary_key=True, default=uuid4)
+
+    class Meta:
+        abstract = True
+
+
+class DatetimeMixin:
+    create_at = models.DateTimeField(_("Created at"), auto_now=False, auto_now_add=True)
+    update_at = models.DateTimeField(_("Updated at"), auto_now=True, auto_now_add=False
+
+                                     )
 class User(models.Model):
     username = models.CharField(_("Username"), max_length=50)
     image = models.ImageField(_("Profile Image"), upload_to='image')
