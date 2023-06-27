550d3d2506299faf1317b95ba401f9129cbb8c1d
bavaaram <itsjustabavaar@gmail.com>
2023-06-27 16:57:59 +0330

    Add required views to users/views.py

--- users/views.py
+++ users/views.py
@@ -1,10 +1,51 @@
-from django.shortcuts import render
-from .models import User
+from django.shortcuts import render, get_object_or_404
+from .models import User, Relation
+from django.views import View
+from django.contrib.auth.models import User
 
-
-def user_list(request):
-    users = User.get_users()
-    context = {'users': users}
-    return render(request, 'user_list.html', context)
 
 # Create your views here.
+
+class UserView(View):
+    def get(self, request):
+        users = User.get_users()
+        context = {'users': users}
+        return render(request, 'user_list.html', context)
+
+
+class ProfileView(View):
+    def get(self, request, username):
+        user = get_object_or_404(User, username=username)
+        profile = user.view_profile()
+        return render(request, "profile.html", profile)
+
+
+class FollowUserView(View):
+    def post(self, request, username):
+        user = get_object_or_404(User, username=username)
+        if request.user.is_authenticated:
+            if request.user != user:
+                if Relation.objects.follow(request.user, user):
+                    message = f'You are now following {username}'
+                else:
+                    message = f'You are already following {username}'
+            else:
+                message = 'You cannot follow yourself'
+        else:
+            message = 'You must be logged in to follow users'
+
+        return render(request, 'follow_user.html', {'message': message})
+
+
+class FollowersView(View):
+    def get(self, request, username):
+        user = get_object_or_404(User, username=username)
+        followers = user.followers_count
+        return render(request, 'followers.html', {'followers': followers})
+
+
+class FollowingsView(View):
+    def get(self, request, username):
+        user = get_object_or_404(User, username=username)
+        followings = user.followings_count
+        return render(request, 'followings.html', {'followings': followings})
