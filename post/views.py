5d28385ad474c432e8676142a7a1549f5f317f91
bavaaram <itsjustabavaar@gmail.com>
2023-06-27 16:55:38 +0330

    Add required views to post/views.py

--- post/views.py
+++ post/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Tag, Image, Comment, Reaction
+from django.views import View
 
-
-def post_list(request):
-    posts = Post.get_posts()
-    context = {'posts': posts}
-    return render(request, 'post_list.html', context)
 
 # Create your views here.
+
+class PostView(View):
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        return render(request, 'post.html', {'post': post})
+
+
+class TagView(View):
+    def get(self, request, tag_id):
+        tag = get_object_or_404(Tag, id=tag_id)
+        return render(request, 'tag.html', {'tag': tag})
+
+
+class ImageView(View):
+    def get(self, request, image_id):
+        image = get_object_or_404(Image, id=image_id)
+        return render(request, 'image.html', {'image': image})
+
+
+class CommentView(View):
+    def get(self, request, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        return render(request, 'comment.html', {'comment': comment})
+
+
+class ReactionView(View):
+    def get(self, request, reaction_id):
+        reaction = get_object_or_404(Reaction, id=reaction_id)
+        return render(request, 'reaction.html', {'reaction': reaction})
