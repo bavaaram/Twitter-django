985c1f3dd527fa8f24027dca5d6030a55eeb9303
bavaaram <itsjustabavaar@gmail.com>
2023-06-27 16:52:01 +0330

    Adding some registers and methods to post/admin.py

--- post/admin.py
+++ post/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Post, Comment, Tag, Reaction, Image
 
 
+# Register your models here.
+
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 1
@@ -9,10 +11,42 @@ class ImageInline(admin.TabularInline):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'text']
-    search_fields = ['username', 'text']
+    fieldsets = (
+        (
+            None, {
+                'fields': ('text', ('user', 'status', 'is_deleted'), 'tags'),
+            },
+        ),
+    )
+    list_display = ['text', 'user', 'status']
+    search_fields = ['text']
+    list_filter = ["status"]
     inlines = [ImageInline]
 
+    @admin.action(description="This Action is Published Selected Post")
+    def published_posts(self, request, queryset):
+        queryset.update(status=Post.Statuses.PUBLISHED)
+        self.message_user(request, 'Published Successfully')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reaction)
+class ReactionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
